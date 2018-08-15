f0f8d50cb72d67b7b622ace83bee8fce16a913f7
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-15 01:29:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,5 @@
 class Student
 
-  class Student
-
 attr_accessor :name, :grade
 attr_reader :id
 
@@ -47,9 +45,7 @@ attr_reader :id
     student
   end
 
-end
-
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
